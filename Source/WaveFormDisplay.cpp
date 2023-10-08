e695792e0194516fca837a48e353d0ced4799353
KeresztesHunor <kereszteshunor36@gmail.com>
2023-10-08 15:27:11 +0200

    g.drawLine coordinate fix

--- Source/WaveFormDisplay.cpp
+++ Source/WaveFormDisplay.cpp
@@ -12,10 +12,10 @@ WaveFormDisplay::~WaveFormDisplay()
 
 void WaveFormDisplay::paint (juce::Graphics& g)
 {
+    g.setColour(juce::Colours::white);
     juce::AudioBuffer<float>& buffer = audioProcessor.getSelectedSamplePanel().getProcessedSampleBuffer();
     if (buffer.getNumChannels() > 0 && buffer.getNumSamples() > 0)
     {
-        g.setColour(juce::Colours::white);
         float sampleChunkSize = buffer.getNumSamples() / static_cast<float>(getWidth());
         int roundedChunkBorderLoewerIndex = 0;
         for (int pixelIndex = 0; pixelIndex < getWidth(); pixelIndex++)
@@ -38,8 +38,13 @@ void WaveFormDisplay::paint (juce::Graphics& g)
             roundedChunkBorderLoewerIndex = roundedChunkBorderUpperIndex;
             float minY = juce::jmap<float>(minValue, -1.f, 1.f, getHeight(), 0.f);
             float maxY = juce::jmap<float>(maxValue, -1.f, 1.f, getHeight(), 0.f);
-            g.drawLine(pixelIndex, maxY, pixelIndex, minY);
+            g.drawLine(pixelIndex, maxY, pixelIndex, minY + 1);
         }
+    }
+    else
+    {
+        int halfHeinght = getHeight() / 2;
+        g.drawLine(0, halfHeinght, getWidth(), halfHeinght);
     }
 }
 
